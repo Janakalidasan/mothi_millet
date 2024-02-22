54d939d529160755e3cda017940360595d02b4cc
janakalidasan T <janakalidasant@gmail.com>
2024-02-23 00:23:02 +0530

    color change

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -12,10 +12,10 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-4 col-md-4 col-sm-12 col-xl-4">
-            <h5>Manage My Account</h5>
+            <h5 style="color:#fff">Manage My Account</h5>
             <div>
-            <a href="{{url('profile-page')}}" style="text-decoration:none; color:#000;">  <p>My Profile</p></a>
-                <a href="{{url('update-profile')}}" style="text-decoration:none; color:#000;"><p>Update profile</p></a>
+            <a  href="{{url('profile-page')}}" style="text-decoration:none; color:#fff;">  <p>My Profile</p></a>
+                <a href="{{url('update-profile')}}" style="text-decoration:none; color:#fff;"><p>Update profile</p></a>
                 <p></p>
             </div>
         </div>
@@ -26,29 +26,29 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="firstname">First Name:</label>
+                        <label for="firstname" style="color:#fff">First Name:</label>
                         <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter first name" value="{{'userId'}}" required>
                     </div>
                     <div class="form-group">
-                        <label for="lastname">Last Name:</label>
+                        <label for="lastname" style="color:#fff">Last Name:</label>
                         <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Enter last name" required>
                     </div>
-                    <div class="form-group ">
-                        <label for="email">Email:</label>
+                    <div class="form-group " >
+                        <label for="email" style="color:#fff">Email:</label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
                     </div>
                     <div class="form-group">
-                        <label for="phone">Phone:</label>
+                        <label for="phone" style="color:#fff">Phone:</label>
                         <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter phone number" required>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="address">Address:</label>
+                        <label for="address" style="color:#fff">Address:</label>
                         <input type="text" class="form-control" id="address" name="address" placeholder="Enter address" required>
                     </div>
                     <div class="form-group">
-                        <label for="altphone">Alternate Phone:</label>
+                        <label for="altphone" style="color:#fff">Alternate Phone:</label>
                         <input type="text" class="form-control" id="altphone" name="altphone" placeholder="Enter alternate phone number">
                     </div>
                 </div>
--- resources/views/user/updateprofile.blade.php
+++ resources/views/user/updateprofile.blade.php
@@ -12,31 +12,31 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-4 col-md-4 col-sm-12 col-xl-4">
-            <h5>Manage My Account</h5>
+            <h5 style="color:#fff">Manage My Account</h5>
             <div>
-            <a href="{{url('profile-page')}}" style="text-decoration:none; color:#000;">  <p>My Profile</p></a>
+            <a href="{{url('profile-page')}}" style="text-decoration:none; color:#fff;">  <p>My Profile</p></a>
               
-                <a href="{{url('update-profile')}}" style="text-decoration:none; color:#000;"><p>Update profile</p></a>
+                <a href="{{url('update-profile')}}" style="text-decoration:none; color:#fff;"><p>Update profile</p></a>
                 <p></p>
             </div>
         </div>
         <div class="col-lg-8 col-md-8 col-sm-12 col-xl-8 mt-4">
-            <p style="color:#DB4444">Update Profile</p>
+            <p style="color:#DB4444" >Update Profile</p>
             <form>
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xl-6">
                         <div class="mb-3">
-                            <label for="examplefirstname" class="form-label">First Name</label>
+                            <label for="examplefirstname" class="form-label" style="color:#fff">First Name</label>
                             <input type="text" class="form-control" id="examplefirstname" aria-describedby="emailHelp"
                                 placeholder="Enter first name">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <label for="exampleInputEmail1" class="form-label" style="color:#fff">Email</label>
                             <input type="email" class="form-control" id="exampleInputEmail1"
                                 aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Phone</label>
+                            <label for="exampleInputEmail1" class="form-label" style="color:#fff">Phone</label>
                             <input type="telegam" class="form-control" id="exampleInputEmail1"
                                 aria-describedby="emailHelp" placeholder="Enter phone no">
                         </div>
@@ -44,17 +44,17 @@
 
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xl-6">
                         <div class="mb-3">
-                            <label for="examplelastname" class="form-label">Last Name</label>
+                            <label for="examplelastname" class="form-label" style="color:#fff">Last Name</label>
                             <input type="text" class="form-control" id="examplelastname" aria-describedby="emailHelp"
                                 placeholder="Enter last name">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleadress" class="form-label">Address</label>
+                            <label for="exampleadress" class="form-label" style="color:#fff">Address</label>
                             <input type="tex" class="form-control" id="exampleadress" aria-describedby="emailHelp"
                                 placeholder="Enter Address">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Alt-Phone</label>
+                            <label for="exampleInputEmail1" class="form-label" style="color:#fff">Alt-Phone</label>
                             <input type="telegam" class="form-control" id="exampleInputEmail1"
                                 aria-describedby="emailHelp" placeholder="Enter Alternate number">
                         </div>
@@ -62,7 +62,7 @@
                 </div>
                 <br>
                 <div class="d-flex justify-content-end align-items-center">
-                    <a href="#" class="btn btn-danger" style="text-decoration:none; color:#000;"><i class="fas fa-times"></i> Cancel</a>&nbsp;&nbsp;
+                    <a href="#" class="btn btn-danger" style="text-decoration:none; color:#fff;"><i class="fas fa-times"></i> Cancel</a>&nbsp;&nbsp;
                     <a href="#" class="btn btn-success"><i class="fas fa-save"></i> update Profile</a>
                 </div>
             </form>
